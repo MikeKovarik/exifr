cd6d363d46bafe8701e1f7379b8accfffb489354
Mike <kenr.mk@gmail.com>
2019-07-05 14:38:01 +0200

    changed default export API

--- index.mjs
+++ index.mjs
@@ -1,2 +1,23 @@
 import {ExifParser} from './src/parser.mjs'
-export default ExifParser
+export {ExifParser}
+export {defaultOptions} from './src/options.mjs'
+
+export async function parse(arg, options) {
+	let parser = new ExifParser(options)
+	await parser.read(arg)
+	if (parser.tiffPosition === undefined) return
+	return parser.parse()
+}
+
+export async function thumbnailBuffer(arg, options = {}) {
+	let parser = new ExifParser(options)
+	await parser.read(arg)
+	if (parser.tiffPosition === undefined) return
+	return parser.extractThumbnail()
+}
+
+export async function thumbnailUrl(...args) {
+	let arrayBuffer = await thumbnailBuffer(...args)
+	let blob = new Blob([arrayBuffer])
+	return URL.createObjectURL(blob)
+}
