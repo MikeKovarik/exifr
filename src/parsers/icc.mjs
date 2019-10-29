a37d395d79dbf0000763652ebeee4a818ed300a5
Mike <kenr.mk@gmail.com>
2019-10-29 22:21:35 +0100

    restructuring of ICC parser

--- src/parsers/icc.mjs
+++ src/parsers/icc.mjs
@@ -9,18 +9,16 @@ import {
 } from '../buff-util.mjs'
 
 
-const APP_ID_OFFSET = 4;
-const APP2_ICC_IDENTIFIER = 'ICC_PROFILE\0';
-const ICC_CHUNK_NUMBER_OFFSET = APP_ID_OFFSET + APP2_ICC_IDENTIFIER.length;
-const ICC_TOTAL_CHUNKS_OFFSET = ICC_CHUNK_NUMBER_OFFSET + 1;
+const PROFILE_HEADER_LENGTH = 84
 
-const PROFILE_HEADER_LENGTH = 84;
+const TAG_TYPE_DESC = 'desc'
+const TAG_TYPE_MLUC = 'mluc'
+const TAG_TYPE_TEXT = 'text'
+const TAG_TYPE_SIG  = 'sig '
 
-var canTranslate = true
+const VALU_EMPTY = '\x00\x00\x00\x00'
 
-console.log('APP2_ICC_IDENTIFIER.length', APP2_ICC_IDENTIFIER.length)
-console.log('ICC_CHUNK_NUMBER_OFFSET', ICC_CHUNK_NUMBER_OFFSET)
-console.log('ICC_TOTAL_CHUNKS_OFFSET', ICC_TOTAL_CHUNKS_OFFSET)
+var canTranslate = true // TODO: pass this through options
 
 export default class IccParser extends AppSegment {
 
@@ -35,17 +33,9 @@ export default class IccParser extends AppSegment {
 	}
 
 	parse() {
-		const {buffer, start, size, end} = this
-		console.log('start', start)
-		console.log('end', end)
-
-		let slice = buffer.slice(start, end)
-		let ab = slice.buffer.slice(slice.byteOffset, slice.byteOffset + slice.byteLength)
-		this.dataView = new DataView(ab)
-		//this.buffView = new BufferView(ab)
-		this.buffView = BufferView.from(this.buffer, this.start, this.size)
-		console.log(this.dataView.toString())
-		console.log(this.buffView.toString())
+		this.view = new BufferView(this.buffer, this.start, this.size)
+		console.log(this.view.toString())
+		console.log(this.view.toString())
 
 		this.output = {}
 		this.parseHeader()
@@ -56,105 +46,83 @@ export default class IccParser extends AppSegment {
 	}
 
 	parseHeader() {
-		const {dataView} = this
-
-		if (dataView.length < PROFILE_HEADER_LENGTH) {
+		if (this.view.byteLength < PROFILE_HEADER_LENGTH)
 			throw new Error('ICC header is too short')
-		}
-
 		for (const [offset, entry] of Object.entries(iccProfile)) {
-			let value = entry.value(dataView, parseInt(offset, 10))
+			/*
+			let value
+			if (headerParsers[offset])
+				value = headerParsers[offset](this.view, offset)
+			else
+				value = this.view.getString(offset, 4)
+			console.log(offset, parser.toString())
+			*/
+			let value = entry.value(this.view, parseInt(offset, 10))
 			if (canTranslate && entry.description) value = entry.description(value) || value
 			this.output[entry.name] = value
 		}
 	}
 
 	parseTags() {
-		const {dataView} = this
-
-		const tagCount = dataView.getUint32(128)
-		this.cursor = 132
-		for (let i = 0; i < tagCount; i++) this.parseNextTag()
+		let tagCount = this.view.getUint32(128)
+		let offset = 132
+		while (tagCount--) {
+			let code = this.view.getString(offset, 4)
+			let value = this.parseTag(offset)
+			// Not all the type parsers are implemented.
+			if (value !== undefined && value !== VALU_EMPTY) this.output[code] = value
+			offset += 12
+		}
 	}
 
-	parseNextTag() {
-		const {dataView} = this
-
-		const code = getStringFromDataView(dataView, this.cursor, 4);
-		const tagOffset = dataView.getUint32(this.cursor + 4);
-		const tagSize = dataView.getUint32(this.cursor + 8);
-		const type = getStringFromDataView(dataView, tagOffset, 4)
-
-		//console.log(type, code, tagOffset, tagSize)
-
+	parseTag(cursor) {
+		let offset = this.view.getUint32(cursor + 4)
+		let size   = this.view.getUint32(cursor + 8)
+		let type   = this.view.getString(offset, 4)
 		switch (type) {
-			case 'desc':
-				this.parseDesc(code, tagOffset)
-				break
-			case 'mluc':
-				this.parseMluc(code, tagOffset)
-				break
-			case 'text':
-				this.parseText(code, tagOffset, tagSize)
-				break
-			case 'sig ':
-				this.parseSig(code, tagOffset)
-				break
-				/*
-			default:
-				this.output[code] = 'TO BE PARSED'
-				*/
+			case TAG_TYPE_DESC: return this.parseDesc(offset)
+			case TAG_TYPE_MLUC: return this.parseMluc(offset)
+			case TAG_TYPE_TEXT: return this.parseText(offset, size)
+			case TAG_TYPE_SIG:  return this.parseSig(offset)
+			// TODO: implement more types
 		}
 
-		this.cursor += 12;
 	}
 
-	parseDesc(code, tagOffset) {
-		let start = tagOffset + 12
-		let size  = this.buffView.getUint32(tagOffset + 8) - 1 // last byte is null termination
-		this.output[code] = this.buffView.getString(start, size)
+	parseDesc(offset) {
+		let size  = this.view.getUint32(offset + 8) - 1 // last byte is null termination
+		return this.view.getString(offset + 12, size).trim()
 	}
 
-	parseText(code, tagOffset, tagSize) {
-		let start = tagOffset + 8
-		let size  = tagSize - 15
-		this.output[code] = this.buffView.getString(start, size)
+	parseText(offset, size) {
+		return this.view.getString(offset + 8, size - 15).trim()
 	}
 
 	// NOTE: some tags end with empty space. TODO: investigate. maybe add .trim() 
-	parseSig(code, tagOffset) {
-		let start = tagOffset + 8
-		this.output[code] = this.buffView.getString(start, 4)
+	parseSig(offset) {
+		return this.view.getString(offset + 8, 4).trim()
 	}
 
 	// Multi Localized Unicode Type
-	parseMluc(code, tagOffset) {
-		console.log('----- MLUC --------')
-		const view = this.buffView
-
-		const recordCount = view.getUint32(tagOffset + 8)
-		const recordSize  = view.getUint32(tagOffset + 12)
-		let offset = tagOffset + 16
-		const val = []
-		for (let recordNum = 0; recordNum < recordCount; recordNum++) {
-			const languageCode = view.getString(offset + 0, 2)
-			const countryCode  = view.getString(offset + 2, 2)
-			const textLength   = view.getUint32(offset + 4)
-			const textOffset   = view.getUint32(offset + 8)
-			const text = view.getUnicodeString(tagOffset + textOffset, textLength)
-			val.push({languageCode, countryCode, text})
-			offset += recordSize
+	parseMluc(tagOffset) {
+		let {view} = this
+		let entryCount = view.getUint32(tagOffset + 8)
+		let entrySize  = view.getUint32(tagOffset + 12)
+		let entryOffset = tagOffset + 16
+		let values = []
+		for (let i = 0; i < entryCount; i++) {
+			let lang    = view.getString(entryOffset + 0, 2)
+			let country = view.getString(entryOffset + 2, 2)
+			let length  = view.getUint32(entryOffset + 4)
+			let offset  = view.getUint32(entryOffset + 8) + tagOffset
+			let text = view.getUnicodeString(offset, length)
+			values.push({lang, country, text})
+			entryOffset += entrySize
 		}
-		if (recordCount === 1) {
-			this.output[code] = val[0].text
-		} else {
-			const valObj = {}
-			for (let i = 0; i < val.length; i++) {
-				let key = `${val[i].languageCode}-${val[i].countryCode}`
-				valObj[key] = val[i].text
-			}
-			this.output[code] = valObj
-		}
+		if (entryCount === 1)
+			return values[0].text
+		else
+			return values
 	}
 
 	translateTags() {
@@ -173,47 +141,6 @@ export default class IccParser extends AppSegment {
 
 
 
-function getStringFromDataView(dataView, offset, length) {
-	const chars = [];
-	for (let i = 0; i < length && offset + i < dataView.byteLength; i++) {
-		chars.push(dataView.getUint8(offset + i, false));
-	}
-	return getAsciiValue(chars).join('');
-}
-
-function getAsciiValue(charArray) {
-	return charArray.map((charCode) => String.fromCharCode(charCode));
-}
-
-function sliceToString(slice) {
-	return String.fromCharCode.apply(null, new Uint8Array(slice))
-}
-
-
-
-
-
-
-
-
-
-
-
-
-const versionMap = {
-	0x02000000: '2.0',
-	0x02100000: '2.1',
-	0x02400000: '2.4',
-	0x04000000: '4.0',
-	0x04200000: '4.2'
-}
-
-const intentMap = {
-	0: 'Perceptual',
-	1: 'Relative',
-	2: 'Saturation',
-	3: 'Absolute'
-}
 
 const valueMap = {
 	// Device
@@ -225,6 +152,7 @@ const valueMap = {
 	spac: 'Space',
 	nmcl: 'Named color',
 }
+
 // Platform
 const companyMap = {
 	appl: 'Apple',
@@ -238,115 +166,142 @@ const companyMap = {
 const iccTags = {
 	desc: 'description',
 	cprt: 'copyright',
-	dmdd: 'deviceModelDescription',
-	vued: 'viewingConditionsDescription',
-	dmnd: 'deviceManufacturerForDisplay',
+	dmdd: 'modelDescription',
+	vued: 'conditionsDescription',
+	dmnd: 'manufacturerForDisplay',
 	tech: 'technology',
 }
 
-/*
-    [4, 'cmm'],
-    [12, 'deviceClass'],
-    [16, 'colorSpace'],
-    [20, 'connectionSpace'],
-    [40, 'platform'],
-    [48, 'manufacturer'],
-    [52, 'model'],
-    [80, 'creator']
-*/
+export const profileTags = {
+	4:  'cmm',
+	8:  'version',
+	12: 'deviceClass',
+	16: 'colorSpace',
+	20: 'connectionSpace',
+	24: 'date',
+	36: 'signature',
+	40: 'platform',
+	48: 'manufacturer',
+	52: 'model',
+	64: 'intent',
+	80: 'creator',
+}
+
+export const headerParsers = {
+	4: parseString,
+	8: parseVersion,
+	12: parseString,
+	16: parseString,
+	20: parseString,
+	24: parseDate,
+	36: parseString,
+	40: parseString,
+	48: parseString,
+	52: parseString,
+	64: (view, offset) => view.getUint32(offset),
+	80: parseString
+}
+
+export const iccTranslations = {
+	4: companyMap,
+	12: {
+		'scnr': 'Input Device profile',
+		'mntr': 'Display Device profile',
+		'prtr': 'Output Device profile',
+		'link': 'DeviceLink profile',
+		'abst': 'Abstract profile',
+		'spac': 'ColorSpace profile',
+		'nmcl': 'NamedColor profile',
+		'cenc': 'ColorEncodingSpace profile',
+		'mid ': 'MultiplexIdentification profile',
+		'mlnk': 'MultiplexLink profile',
+		'mvis': 'MultiplexVisualization profile',
+	},
+	40: companyMap,
+	48: companyMap,
+	64: {
+		0: 'Perceptual',
+		1: 'Relative Colorimetric',
+		2: 'Saturation',
+		3: 'Absolute Colorimetric',
+	},
+}
+
 
 export const iccProfile = {
 	4: {
-		'name': 'Preferred CMM type',
-		'value': (dataView, offset) => getStringFromDataView(dataView, offset, 4),
-		'description': (value) => companyMap[value] || undefined,
+		'name': profileTags[4],
+		'value': headerParsers[4],
+		'description': (value) => iccTranslations[4][value],
 	},
 	8: {
-		'name': 'Profile Version',
-		'value': (dataView, offset) => {
-			return (dataView.getUint8(offset)).toString(10) + '.'
-			+ (dataView.getUint8(offset + 1) >> 4).toString(10) + '.'
-			+ (dataView.getUint8(offset + 1) % 16).toString(10);
-		}
+		'name': profileTags[8],
+		'value': headerParsers[8],
 	},
 	12: {
-		'name': 'Profile/Device class',
-		'value': (dataView, offset) => getStringFromDataView(dataView, offset, 4),
-		'description': (value) => {
-			switch (value.toLowerCase()) {
-				case 'scnr': return 'Input Device profile';
-				case 'mntr': return 'Display Device profile';
-				case 'prtr': return 'Output Device profile';
-				case 'link': return 'DeviceLink profile';
-				case 'abst': return 'Abstract profile';
-				case 'spac': return 'ColorSpace profile';
-				case 'nmcl': return 'NamedColor profile';
-				case 'cenc': return 'ColorEncodingSpace profile';
-				case 'mid ': return 'MultiplexIdentification profile';
-				case 'mlnk': return 'MultiplexLink profile';
-				case 'mvis': return 'MultiplexVisualization profile';
-				default: return value;
-			}
-		}
+		'name': profileTags[12],
+		'value': headerParsers[12],
+		'description': (value) => iccTranslations[12][value],
 	},
 	16: {
-		'name': 'Color Space',
-		'value': (dataView, offset) => getStringFromDataView(dataView, offset, 4)
+		'name': profileTags[16],
+		'value': headerParsers[16],
 	},
 	20: {
-		'name': 'Connection Space',
-		'value': (dataView, offset) => getStringFromDataView(dataView, offset, 4)
+		'name': profileTags[20],
+		'value': headerParsers[20],
 	},
 	24: {
-		'name': 'ICC Profile Date',
-		'value': (dataView, offset) => parseDate(dataView, offset).toISOString()
+		'name': profileTags[24],
+		'value': headerParsers[24],
 	},
 	36: {
-		'name': 'ICC Signature',
-		'value': (dataView, offset) => sliceToString(dataView.buffer.slice(offset, offset + 4))
+		'name': profileTags[36],
+		'value': headerParsers[36],
 	},
 	40: {
-		'name': 'Primary Platform',
-		'value': (dataView, offset) => getStringFromDataView(dataView, offset, 4),
-		'description': (value) => companyMap[value]
+		'name': profileTags[40],
+		'value': headerParsers[40],
+		'description': (value) => iccTranslations[40][value],
 	},
 	48: {
-		'name': 'Device Manufacturer',
-		'value': (dataView, offset) => getStringFromDataView(dataView, offset, 4),
-		'description': (value) => companyMap[value]
+		'name': profileTags[48],
+		'value': headerParsers[48],
+		'description': (value) => iccTranslations[48][value],
 	},
 	52: {
-		'name': 'Device Model Number',
-		'value': (dataView, offset) => getStringFromDataView(dataView, offset, 4)
+		'name': profileTags[52],
+		'value': headerParsers[52],
 	},
 	64: {
-		'name': 'Rendering Intent',
-		'value': (dataView, offset) => dataView.getUint32(offset),
-		'description': (value) => {
-			switch (value) {
-				case 0: return 'Perceptual';
-				case 1: return 'Relative Colorimetric';
-				case 2: return 'Saturation';
-				case 3: return 'Absolute Colorimetric';
-				default: return value;
-			}
-		}
+		'name': profileTags[64],
+		'value': headerParsers[64],
+		'description': (value) => iccTranslations[64][value],
 	},
-
 	80: {
-		'name': 'Profile Creator',
-		'value': (dataView, offset) => getStringFromDataView(dataView, offset, 4)
+		'name': profileTags[80],
+		'value': headerParsers[80]
 	},
-};
+}
 
-function parseDate(dataView, offset) {
-	const year = dataView.getUint16(offset);
-	const month = dataView.getUint16(offset + 2) - 1;
-	const day = dataView.getUint16(offset + 4);
-	const hours = dataView.getUint16(offset + 6);
-	const minutes = dataView.getUint16(offset + 8);
-	const seconds = dataView.getUint16(offset + 10);
-	return new Date(Date.UTC(year, month, day, hours, minutes, seconds));
+function parseString(view, offset) {
+	return view.getString(offset, 4).trim()
+}
+
+function parseVersion(view, offset) {
+	return (view.getUint8(offset)).toString(10)
+		+ '.' + (view.getUint8(offset + 1) >> 4).toString(10)
+		+ '.' + (view.getUint8(offset + 1) % 16).toString(10)
+}
+
+function parseDate(view, offset) {
+	const year    = view.getUint16(offset)
+	const month   = view.getUint16(offset + 2) - 1
+	const day     = view.getUint16(offset + 4)
+	const hours   = view.getUint16(offset + 6)
+	const minutes = view.getUint16(offset + 8)
+	const seconds = view.getUint16(offset + 10)
+	return new Date(Date.UTC(year, month, day, hours, minutes, seconds))
 }
 
 
