370080fbb67f9ccd904d883b0397f3e9dc3b4fa3
Mike <kenr.mk@gmail.com>
2019-11-25 18:21:39 +0100

    fixed flaky test

--- test/parser.spec.js
+++ test/parser.spec.js
@@ -6,27 +6,33 @@ import {parse, Exifr} from '../src/index-full.js'
 
 describe('parser', () => {
 
-    it(`.findJpgAppSegments() finds jpeg APP segments in jpg file`, async () => {
-        let input = await getFile('IMG_20180725_163423.jpg')
-        let exifr = new Exifr()
-        await exifr.read(input)
-        exifr.findJpgAppSegments()
-        exifr.options = {tiff: true, xmp: true, jfif: true}
-        let jfifSegment = exifr.appSegments.find(segment => segment.type === 'jfif')
-        assert.isDefined(jfifSegment)
-        assert.equal(jfifSegment.offset, 25388)
-        assert.equal(jfifSegment.length, 18)
-        assert.equal(jfifSegment.start, 25397)
-        assert.equal(jfifSegment.size, 9)
-        assert.equal(jfifSegment.end, 25406)
-		/*
-        let tiffSegment = exifr.appSegments.find(segment => segment.type === 'tiff')
-        assert.isDefined(tiffSegment)
-        let xmpSegment = exifr.appSegments.find(segment => segment.type === 'xmp')
-        assert.isDefined(xmpSegment)
-		*/
-    })
+	describe('exifr.findJpgAppSegments()', () => {
 
-	// TODO: tests for tif
+		it(`finds APP segments existing in jpg file`, async () => {
+			let input = await getFile('IMG_20180725_163423.jpg')
+			let exifr = new Exifr({tiff: true, xmp: true, jfif: true})
+			await exifr.read(input)
+			exifr.findJpgAppSegments()
+			let jfifSegment = exifr.appSegments.find(segment => segment.type === 'jfif')
+			assert.isDefined(jfifSegment)
+			assert.equal(jfifSegment.offset, 25388)
+			assert.equal(jfifSegment.length, 18)
+			assert.equal(jfifSegment.start, 25397)
+			assert.equal(jfifSegment.size, 9)
+			assert.equal(jfifSegment.end, 25406)
+			let tiffSegment = exifr.appSegments.find(segment => segment.type === 'tiff')
+			assert.isDefined(tiffSegment)
+		})
+
+		it(`doesn't find segment not present in jpg file`, async () => {
+			let input = await getFile('IMG_20180725_163423.jpg')
+			let exifr = new Exifr({tiff: true, xmp: true, jfif: true})
+			await exifr.read(input)
+			exifr.findJpgAppSegments()
+			let xmpSegment = exifr.appSegments.find(segment => segment.type === 'xmp')
+			assert.isUndefined(xmpSegment)
+		})
+
+	})
 
 })
