c0a0ae5f2dd867f13dfc73d0fe996e295c8915d8
Mike <kenr.mk@gmail.com>
2019-12-29 00:09:52 +0100

    updated icc tests to match dictionary; updated some values, chores

--- debug/icc.mjs
+++ debug/icc.mjs
@@ -2,8 +2,10 @@
 import Exifr from '../src/index-full.js'
 import {promises as fs} from 'fs'
 
+//fs.readFile('../test/fixtures/issue-fast-exif-2.jpg')
 //fs.readFile('../test/fixtures/IMG_20180725_163423.jpg')
-fs.readFile('../test/fixtures/issue-fast-exif-2.jpg')
+//fs.readFile('../test/fixtures/Bush-dog.jpg')
+fs.readFile('../test/fixtures/tif-with-iptc-icc-xmp.tif')
 	.then(async buffer => {
 		const output = await Exifr.parse(buffer, {icc: true, mergeOutput: false})
 		console.log('output', output.icc)
--- test/icc.spec.js
+++ test/icc.spec.js
@@ -33,16 +33,16 @@ describe('ICC Segment', () => {
 		file: 'Bush-dog.jpg',
 		tags: [
 			[
-				80, 'creator',
+				80, 'ProfileCreator',
 				'HP', 'Hewlett-Packard',
 			], [
-				12, 'deviceClass',
+				12, 'ProfileClass',
 				'mntr', 'Monitor',
 			], [
-				40, 'platform',
+				40, 'PrimaryPlatform',
 				'MSFT', 'Microsoft',
 			], [
-				'cprt', 'copyright',
+				'cprt', 'ProfileCopyright',
 				'Copyright (c) 1998 Hewlett-Packard C',
 			]
 		]
@@ -76,116 +76,147 @@ describe('ICC Segment', () => {
 	describe('IccParser class', () => {
 
 		testProfile('D65_XYZ.icc', {
-			cmm: 'none',
-			version: '2.4.0',
-			deviceClass: 'Monitor',
-			colorSpace: 'RGB',
-			connectionSpace: 'XYZ',
-			//date: 2004-07-21T18:57:42.000Z,
-			signature: 'acsp',
-			manufacturer: 'none',
-			model: 'none',
-			intent: 'Relative Colorimetric',
-			creator: 'none',
-			description: 'D65 XYZ profile',
-			modelDescription: 'IEC 61966-2-1 Default RGB Colour Space - sRGB',
-			technology: 'CRT',
-			conditionsDescription: 'Reference Viewing Condition in IEC 61966-2-1',
-			copyright: 'Copyright Hewlett Packard'
+			ProfileCMMType: 'none',
+			ProfileVersion: '2.4.0',
+			ProfileClass: 'Monitor',
+			ColorSpaceData: 'RGB',
+			ProfileConnectionSpace: 'XYZ',
+			//ProfileDateTime: 2004-07-21T18:57:42.000Z,
+			ProfileFileSignature: 'acsp',
+			DeviceManufacturer: 'none',
+			DeviceModel: 'none',
+			RenderingIntent: 'Relative Colorimetric',
+			ProfileCreator: 'none',
+			ProfileDescription: 'D65 XYZ profile',
+			DeviceModelDesc: 'IEC 61966-2-1 Default RGB Colour Space - sRGB',
+			Technology: 'Cathode Ray Tube Display',
+			ViewingCondDesc: 'Reference Viewing Condition in IEC 61966-2-1',
+			ProfileCopyright: 'Copyright Hewlett Packard'
 		})
 
 		testProfile('sRGB_IEC61966-2-1_black_scaled.icc', {
-			version: '2.0.0',
-			deviceClass: 'Monitor',
-			colorSpace: 'RGB',
-			connectionSpace: 'XYZ',
-			//date: 2009-03-27T21:36:31.000Z,
-			signature: 'acsp',
-			intent: 'Perceptual',
-			description: 'sRGB IEC61966-2-1 black scaled',
-			modelDescription: 'IEC 61966-2-1 Default RGB Colour Space - sRGB',
-			technology: 'CRT',
-			conditionsDescription: 'Reference Viewing Condition in IEC 61966-2-1',
-			copyright: 'Copyright International Color Consortium'
+			ProfileVersion: '2.0.0',
+			ProfileClass: 'Monitor',
+			ColorSpaceData: 'RGB',
+			ProfileConnectionSpace: 'XYZ',
+			//ProfileDateTime: 2009-03-27T21:36:31.000Z,
+			ProfileFileSignature: 'acsp',
+			RenderingIntent: 'Perceptual',
+			ProfileDescription: 'sRGB IEC61966-2-1 black scaled',
+			DeviceModelDesc: 'IEC 61966-2-1 Default RGB Colour Space - sRGB',
+			Technology: 'Cathode Ray Tube Display',
+			ViewingCondDesc: 'Reference Viewing Condition in IEC 61966-2-1',
+			ProfileCopyright: 'Copyright International Color Consortium'
 		})
 
 		testProfile('sRGB_v4_ICC_preference.icc', {
-			version: '4.2.0',
-			deviceClass: 'Space',
-			colorSpace: 'RGB',
-			connectionSpace: 'Lab',
-			//date: 2007-07-25T00:05:37.000Z,
-			signature: 'acsp',
-			intent: 'Perceptual',
-			description: 'sRGB v4 ICC preference perceptual intent beta',
-			rig0: 'prmg',
-			copyright: 'Copyright 2007 International Color Consortium'
+			ProfileVersion: '4.2.0',
+			ProfileClass: 'Color Space Conversion Profile',
+			ColorSpaceData: 'RGB',
+			ProfileConnectionSpace: 'Lab',
+			//ProfileDateTime: 2007-07-25T00:05:37.000Z,
+			ProfileFileSignature: 'acsp',
+			RenderingIntent: 'Perceptual',
+			ProfileDescription: 'sRGB v4 ICC preference perceptual intent beta',
+			PerceptualRenderingIntentGamut: 'prmg',
+			ProfileCopyright: 'Copyright 2007 International Color Consortium'
 		})
 
 		testProfile('sRGB2014.icc', {
-			version: '2.0.0',
-			deviceClass: 'Monitor',
-			colorSpace: 'RGB',
-			connectionSpace: 'XYZ',
-			//date: 2015-02-15T00:00:00.000Z,
-			signature: 'acsp',
-			intent: 'Perceptual',
-			description: 'sRGB2014',
-			modelDescription: 'IEC 61966-2-1 Default RGB Colour Space - sRGB',
-			technology: 'CRT',
-			conditionsDescription: 'Reference Viewing Condition in IEC 61966-2-1',
-			copyright: 'Copyright International Color Consortium'
+			ProfileVersion: '2.0.0',
+			ProfileClass: 'Monitor',
+			ColorSpaceData: 'RGB',
+			ProfileConnectionSpace: 'XYZ',
+			//ProfileDateTime: 2015-02-15T00:00:00.000Z,
+			ProfileFileSignature: 'acsp',
+			RenderingIntent: 'Perceptual',
+			ProfileDescription: 'sRGB2014',
+			DeviceModelDesc: 'IEC 61966-2-1 Default RGB Colour Space - sRGB',
+			Technology: 'Cathode Ray Tube Display',
+			ViewingCondDesc: 'Reference Viewing Condition in IEC 61966-2-1',
+			ProfileCopyright: 'Copyright International Color Consortium'
 		})
 
 		testProfile('USWebCoatedSWOP.icc', {
-			cmm: 'ADBE',
-			version: '2.1.0',
-			deviceClass: 'Printer',
-			colorSpace: 'CMYK',
-			connectionSpace: 'Lab',
-			//date: 2000-07-26T05:41:53.000Z,
-			signature: 'acsp',
-			platform: 'Apple',
-			manufacturer: 'Adobe',
-			intent: 'Perceptual',
-			creator: 'Adobe',
-			description: 'U.S. Web Coated (SWOP) v2',
-			copyright: 'Copyright 2000 Adobe Systems'
+			ProfileCMMType: 'Adobe',
+			ProfileVersion: '2.1.0',
+			ProfileClass: 'Printer',
+			ColorSpaceData: 'CMYK',
+			ProfileConnectionSpace: 'Lab',
+			//ProfileDateTime: 2000-07-26T05:41:53.000Z,
+			ProfileFileSignature: 'acsp',
+			PrimaryPlatform: 'Apple Computer',
+			DeviceManufacturer: 'Adobe',
+			RenderingIntent: 'Perceptual',
+			ProfileCreator: 'Adobe',
+			ProfileDescription: 'U.S. Web Coated (SWOP) v2',
+			ProfileCopyright: 'Copyright 2000 Adobe Systems'
 		})
 
-	})
-
-	testImage('icc', 'IMG_20180725_163423.jpg', {
-		cmm: undefined, // we intentionally do not include empty \u0000 values
-		version: '4.0.0',
-		intent: 'Perceptual',
-		colorSpace: 'RGB',
-		creator: 'Google',
-		description: 'sRGB IEC61966-2.1\u0000',
-		copyright: 'Copyright (c) 2016 Google Inc.\u0000',
-	})
-
-	testImage('icc', 'Bush-dog.jpg', {
-		version: '2.1.0',
-		intent: 'Perceptual',
-		cmm: 'Lino',
-		deviceClass: 'Monitor',
-		colorSpace: 'RGB',
-		connectionSpace: 'XYZ',
-		platform: 'Microsoft',
-		manufacturer: 'IEC',
-		model: 'sRGB',
-		creator: 'Hewlett-Packard',
-		copyright: 'Copyright (c) 1998 Hewlett-Packard C',
 	})
 
 	testImage('icc', 'issue-fast-exif-2.jpg', {
 		// TEXT tag
-		copyright: 'Copyright (c) 1998 Hewlett-Packard C',
+		ProfileCopyright: 'Copyright (c) 1998 Hewlett-Packard C',
 		// SIG tag
-		technology: 'CRT',
-		//
-		conditionsDescription: 'Reference Viewing Condition in IEC61966-2.1',
+		Technology: 'Cathode Ray Tube Display',
+		// ???
+		ViewingCondDesc: 'Reference Viewing Condition in IEC61966-2.1',
+	})
+
+	testImage('icc', 'IMG_20180725_163423.jpg', {
+		ProfileCMMType: undefined, // we intentionally do not include empty \u0000 values
+		ProfileVersion: '4.0.0',
+		ProfileClass: 'Monitor',
+		ColorSpaceData: 'RGB',
+		ProfileConnectionSpace: 'XYZ',
+		//ProfileDateTime: 2016-12-08T09:38:28.000Z,
+		ProfileFileSignature: 'acsp',
+		DeviceManufacturer: 'Google',
+		RenderingIntent: 'Perceptual',
+		ProfileCreator: 'Google',
+		ProfileDescription: 'sRGB IEC61966-2.1\u0000',
+		ProfileCopyright: 'Copyright (c) 2016 Google Inc.\u0000'
+	})
+
+	testImage('icc', 'Bush-dog.jpg', {
+		ProfileCMMType: 'Linotronic',
+		ProfileVersion: '2.1.0',
+		ProfileClass: 'Monitor',
+		ColorSpaceData: 'RGB',
+		ProfileConnectionSpace: 'XYZ',
+		//ProfileDateTime: 1998-02-09T06:49:00.000Z,
+		ProfileFileSignature: 'acsp',
+		PrimaryPlatform: 'Microsoft',
+		DeviceManufacturer: 'Hewlett-Packard',
+		DeviceModel: 'sRGB',
+		RenderingIntent: 'Perceptual',
+		ProfileCreator: 'Hewlett-Packard',
+		ProfileCopyright: 'Copyright (c) 1998 Hewlett-Packard C',
+		ProfileDescription: 'sRGB IEC61966-2.1',
+		DeviceMfgDesc: 'IEC http://www.iec.ch',
+		DeviceModelDesc: 'IEC 61966-2.1 Default RGB colour space - sRGB',
+		ViewingCondDesc: 'Reference Viewing Condition in IEC61966-2.1',
+		Technology: 'Cathode Ray Tube Display'
+	})
+
+	testImage('icc', 'tif-with-iptc-icc-xmp.tif', {
+		ProfileCMMType: 'KCMS',
+		ProfileVersion: '2.1.0',
+		ProfileClass: 'Monitor',
+		ColorSpaceData: 'RGB',
+		ProfileConnectionSpace: 'XYZ',
+		//ProfileDateTime: '1998-12-01T18:58:21.000Z',
+		ProfileFileSignature: 'acsp',
+		PrimaryPlatform: 'Microsoft',
+		DeviceManufacturer: 'Kodak',
+		DeviceModel: 'ROMM',
+		RenderingIntent: 'Perceptual',
+		ProfileCreator: 'Kodak',
+		ProfileCopyright: 'Copyright (c) Eastman Kodak Company, 1999, all rights res',
+		ProfileDescription: 'ProPhoto RGB',
+		DeviceMfgDesc: 'KODAK',
+		DeviceModelDesc: 'Reference Output Medium Metric(ROMM)'
 	})
 
 })
