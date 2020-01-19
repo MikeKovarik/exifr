208357861d3bd18a477da4b860124ea357f005cf
Mike <kenr.mk@gmail.com>
2020-01-20 00:34:54 +0100

    improved XMP example file

--- examples/xmp.js
+++ examples/xmp.js
@@ -2,11 +2,17 @@ import Exifr from '../index.mjs'
 import {promises as fs} from 'fs'
 
 
+// Exifr does not come with XMP parser out of the box, because those are heavy & complicated
+// and not everyone needs to parse XMP/XML. So to keep exifr simple, we just extract the XMP string
+// and you have to parse it yourself.
+
+// Exifr offers you an API for using your own XML parser while parsing XMP.
+// 1) get the XmlParser class.
 let XmpParser = Exifr.segmentParsers.get('xmp')
-console.log('-: XmpParser', XmpParser)
-XmpParser.parseXml = function(string) {
-	let parsedXml = string + ':)' // do something with the xml here
-	return parsedXml
+// 2) Implement parseXml() method which takes one string argument
+//    and returns anything that ends up as output.xmp.
+XmpParser.prototype.parseXml = function(xmpString) {
+	return 'Bring Your Own XML parser here: ' + xmpString
 }
 
 // only extract the XMP segment and nothing more than is necessary.
