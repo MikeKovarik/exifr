e8d65b7b184766c76285a8b9e653c81bfce5757d
Mike <kenr.mk@gmail.com>
2020-02-01 14:09:40 +0100

    added custom build example

---
+++ examples/custom-build.js
@@ -0,0 +1,18 @@
+import * as exifr from '../src/index-core.js'
+// We're in node.js so we'll be reading the file using 'fs' module
+import '../src/file-readers/FsReader.js'
+// We only have jpg files
+import '../src/file-parsers/jpeg.js'
+// And we're only parsing IPTC segment
+import '../src/segment-parsers/iptc.js'
+// So we only include IPTC dictionaries
+import '../src/dicts/iptc-keys.js'
+import '../src/dicts/iptc-values.js'
+
+// We still need to tell exifr to not look for TIFF, which is true by default.
+// And also enable IPTC which is disabled by default.
+let options = {tiff: false, iptc: true}
+let filePath = '../test/fixtures/IMG_20180725_163423.jpg'
+exifr.parse(filePath, options).then(output => {
+	console.log('output', output)
+})
