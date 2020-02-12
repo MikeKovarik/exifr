e1f7ebd2698781aa4b34fb8722ad02879384ad0a
Mike <kenr.mk@gmail.com>
2020-02-12 19:46:46 +0100

    demopage: loads src/ on localhost or dist/ in production

--- homepage/app.js
+++ homepage/app.js
@@ -1,6 +1,4 @@
-import * as exifr from '../dist/full.esm.js'
-import {Exifr, Options, readBlobAsArrayBuffer} from '../dist/full.esm.js'
-import {segmentsAndBlocks, tiffExtractables} from '../dist/full.esm.js'
+var exifr
 import {JsonValueConverter} from './json-beautifier.js'
 import cloneObject from './clone.js'
 import {SegmentBoxCustomElement, ObjectTableCustomElement} from './components.js'
@@ -15,26 +13,42 @@ let demoFileSize = 311406
 class ExifrDemoApp {
 
 	constructor() {
-		this.setupDom().catch(this.handleError)
-		this.setupExifr().catch(this.handleError)
+		this.setup().catch(this.handleError)
 	}
 
-	async setupDom() {
+	async setup() {
 		this.thumbImg = document.querySelector('#thumb img')
 
 		// dropzone
 		document.body.addEventListener('dragenter', e => e.preventDefault())
 		document.body.addEventListener('dragover', e => e.preventDefault())
 		document.body.addEventListener('drop', this.onDrop)
-	}
 
-	async setupExifr() {
-		this.options = cloneObject(Options.default)
+		if (window.location.origin.includes('localhost'))
+			exifr = await import('../src/bundle-full.js')
+		else
+			exifr = await import('../dist/full.esm.js')
+
+		this.options = cloneObject(exifr.Options.default)
 		this.options.ifd1 = true
 		// Load the demo image as array buffer to keep in memory
 		// to prevent distortion of initial parse time.
 		// i.e: show off library's performance and don't include file load time in it.
 		this.loadPhoto(demoFileName, demoFileSize)
+	}
+
+	async loadPhoto(fileName, expectedFileSize) {
+		this.setStatus('Loading image')
+		let filePath = fixtureDirPath + fileName
+		let res = await fetch(filePath)
+		let fetchedSize = Number(res.headers.get('content-length'))
+		if (fetchedSize === expectedFileSize) {
+			// TODO expectedFileSize
+			let file = await res.arrayBuffer()
+			this.parseFile(file)
+		} else {
+			this.setStatus(`Demo image couldn't load: You are using data saver.`, 'red')
+		}
 	}
 
 	handleError = err => {
@@ -43,7 +57,7 @@ class ExifrDemoApp {
 	}
 
 	toggleAllOptions() {
-		let keys = [...segmentsAndBlocks, ...tiffExtractables]
+		let keys = [...exifr.segmentsAndBlocks, ...exifr.tiffExtractables]
 		let values = keys.map(key => this.options[key])
 		let hasSomethingUnchecked = values.some(val => val === false)
 		for (let key of keys)
@@ -62,20 +76,6 @@ class ExifrDemoApp {
 		this.parseFile()
 	}
 
-	async loadPhoto(fileName, expectedFileSize) {
-		this.setStatus('Loading image')
-		let filePath = fixtureDirPath + fileName
-		let res = await fetch(filePath)
-		let fetchedSize = Number(res.headers.get('content-length'))
-		if (fetchedSize === expectedFileSize) {
-			// TODO expectedFileSize
-			let file = await res.arrayBuffer()
-			this.parseFile(file)
-		} else {
-			this.setStatus(`Demo image couldn't load: You are using data saver.`, 'red')
-		}
-	}
-
 	onDrop = async e => {
 		e.preventDefault()
 		this.processBlob(e.dataTransfer.files[0])
@@ -86,7 +86,7 @@ class ExifrDemoApp {
 	}
 
 	async processBlob(blob) {
-		let file = await readBlobAsArrayBuffer(blob)
+		let file = await exifr.readBlobAsArrayBuffer(blob)
 		this.parseFile(file)
 	}
 
@@ -131,7 +131,7 @@ class ExifrDemoApp {
 		options.sanitize = true
 		options.translateKeys = true
 		// paarse
-		let exr = new Exifr(options)
+		let exr = new exifr.Exifr(options)
 		await exr.read(input)
 		let output = await exr.parse() || {}
 		this.output = output
