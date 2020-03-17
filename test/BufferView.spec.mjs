0ff0add3a2eed0547698da85451941123995a523
Mike <kenr.mk@gmail.com>
2020-03-17 19:02:09 +0100

    added DataView & ArrayBuffer input tests for BufferView

--- test/BufferView.spec.mjs
+++ test/BufferView.spec.mjs
@@ -4,11 +4,26 @@ import {isNode, getPath} from './test-util-core.mjs'
 import {BufferView} from '../src/util/BufferView.mjs'
 
 
+// Node.js tries to share memory of newly created Buffer instances in shared large ArrayBuffer.
+// This function creates new blank slate.
+function createIsolatedArrayBuffer(...bytes) {
+	let arrayBuffer = new ArrayBuffer(bytes.length)
+	let buffer = new Uint8Array(arrayBuffer)
+	for (let i = 0; i < bytes.length; i++)
+		buffer[i] = bytes[i]
+	return buffer.buffer
+}
+
+function createIsolatedDataView(...bytes) {
+	let arrayBuffer = createIsolatedArrayBuffer(...bytes)
+	return new DataView(arrayBuffer)
+}
+
 describe('BufferView', () => {
 
 	describe('constructor new BufferView', () => {
 
-		it(`Uint8Array`, async () => {
+		it(`(Uint8Array)`, async () => {
 			let uint8 = new Uint8Array(3)
 			uint8[2] = 5
 			let view = new BufferView(uint8)
@@ -19,17 +34,93 @@ describe('BufferView', () => {
 			assert.equal(view.getUint8(2), 5)
 		})
 
-		it(`number: creates new view`, async () => {
+		it(`(number) creates new view`, async () => {
 			let view = new BufferView(3)
 			assert.equal(view.byteLength, 3)
 		})
 
-		it(`number: creates new view`, async () => {
+		it(`(number) creates new view`, async () => {
 			let view = new BufferView(3)
 			assert.equal(view.byteLength, 3)
 		})
 
-		isNode && it(`Buffer.allocUnsafe: creates new view`, async () => {
+		it(`(ArrayBuffer) creates new view`, async () => {
+			let arrayBuffer = createIsolatedArrayBuffer(0, 1, 2, 3, 4)
+			let view = new BufferView(arrayBuffer)
+			assert.equal(view.byteLength, 5)
+		})
+
+		it(`(ArrayBuffer, 0, 3) creates subview`, async () => {
+			let arrayBuffer = createIsolatedArrayBuffer(0, 1, 2, 3, 4)
+			let view = new BufferView(arrayBuffer, 0, 3)
+			assert.equal(view.byteLength, 3)
+			assert.equal(view.getUint8(0), 0)
+			assert.equal(view.getUint8(1), 1)
+			assert.equal(view.getUint8(2), 2)
+		})
+
+		it(`(ArrayBuffer, 1, 3) creates subview`, async () => {
+			let arrayBuffer = createIsolatedArrayBuffer(0, 1, 2, 3, 4)
+			let view = new BufferView(arrayBuffer, 1, 3)
+			assert.equal(view.byteLength, 3)
+			assert.equal(view.getUint8(0), 1)
+			assert.equal(view.getUint8(1), 2)
+			assert.equal(view.getUint8(2), 3)
+		})
+
+		it(`(ArrayBuffer, 2, 3) creates subview`, async () => {
+			let arrayBuffer = createIsolatedArrayBuffer(0, 1, 2, 3, 4)
+			let view = new BufferView(arrayBuffer, 2, 3)
+			assert.equal(view.byteLength, 3)
+			assert.equal(view.getUint8(0), 2)
+			assert.equal(view.getUint8(1), 3)
+			assert.equal(view.getUint8(2), 4)
+		})
+
+		it(`(ArrayBuffer, 3, 3) throws out of range`, async () => {
+			let arrayBuffer = createIsolatedArrayBuffer(0, 1, 2, 3, 4)
+			assert.throws(() => new BufferView(arrayBuffer, 3, 3))
+		})
+
+		it(`(DataView) creates new view`, async () => {
+			let arrayBuffer = createIsolatedDataView(0, 1, 2, 3, 4)
+			let view = new BufferView(arrayBuffer)
+			assert.equal(view.byteLength, 5)
+		})
+
+		it(`(DataView, 0, 3) creates subview`, async () => {
+			let arrayBuffer = createIsolatedDataView(0, 1, 2, 3, 4)
+			let view = new BufferView(arrayBuffer, 0, 3)
+			assert.equal(view.byteLength, 3)
+			assert.equal(view.getUint8(0), 0)
+			assert.equal(view.getUint8(1), 1)
+			assert.equal(view.getUint8(2), 2)
+		})
+
+		it(`(DataView, 1, 3) creates subview`, async () => {
+			let arrayBuffer = createIsolatedDataView(0, 1, 2, 3, 4)
+			let view = new BufferView(arrayBuffer, 1, 3)
+			assert.equal(view.byteLength, 3)
+			assert.equal(view.getUint8(0), 1)
+			assert.equal(view.getUint8(1), 2)
+			assert.equal(view.getUint8(2), 3)
+		})
+
+		it(`(DataView, 2, 3) creates subview`, async () => {
+			let arrayBuffer = createIsolatedDataView(0, 1, 2, 3, 4)
+			let view = new BufferView(arrayBuffer, 2, 3)
+			assert.equal(view.byteLength, 3)
+			assert.equal(view.getUint8(0), 2)
+			assert.equal(view.getUint8(1), 3)
+			assert.equal(view.getUint8(2), 4)
+		})
+
+		it(`(DataView, 3, 3) throws out of range`, async () => {
+			let arrayBuffer = createIsolatedDataView(0, 1, 2, 3, 4)
+			assert.throws(() => new BufferView(arrayBuffer, 3, 3))
+		})
+
+		isNode && it(`(Buffer.allocUnsafe) creates new view`, async () => {
 			let buffer = Buffer.allocUnsafe(3)
 			let val0 = buffer[0]
 			let val1 = buffer[1]
@@ -41,7 +132,7 @@ describe('BufferView', () => {
 			assert.equal(view.getUint8(2), val2)
 		})
 
-		isNode && it(`Buffer.allocUnsafe, 0, 3: creates subview`, async () => {
+		isNode && it(`(Buffer.allocUnsafe, 0, 3) creates subview`, async () => {
 			let buffer = Buffer.allocUnsafe(5)
 			let val0 = buffer[0]
 			let val1 = buffer[1]
@@ -53,7 +144,7 @@ describe('BufferView', () => {
 			assert.equal(view.getUint8(2), val2)
 		})
 
-		isNode && it(`Buffer.allocUnsafe, 1, 3: creates subview`, async () => {
+		isNode && it(`(Buffer.allocUnsafe, 1, 3) creates subview`, async () => {
 			let buffer = Buffer.allocUnsafe(5)
 			let val1 = buffer[1]
 			let val2 = buffer[2]
@@ -65,7 +156,7 @@ describe('BufferView', () => {
 			assert.equal(view.getUint8(2), val3)
 		})
 
-		isNode && it(`Buffer.allocUnsafe, 2, 3: creates subview`, async () => {
+		isNode && it(`(Buffer.allocUnsafe, 2, 3) creates subview`, async () => {
 			let buffer = Buffer.allocUnsafe(5)
 			let val2 = buffer[2]
 			let val3 = buffer[3]
@@ -75,6 +166,11 @@ describe('BufferView', () => {
 			assert.equal(view.getUint8(0), val2)
 			assert.equal(view.getUint8(1), val3)
 			assert.equal(view.getUint8(2), val4)
+		})
+
+		isNode && it(`(Buffer.allocUnsafe, 3, 3) throws out of range`, async () => {
+			let buffer = Buffer.allocUnsafe(5)
+			assert.throws(() => new BufferView(buffer, 3, 3))
 		})
 
 	})
