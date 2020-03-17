9c43cbdcf10101052dbcf9a67eb617db4e7d4fe4
Mike <kenr.mk@gmail.com>
2020-03-17 18:39:59 +0100

    restructured BufferView tests

--- test/BufferView.spec.mjs
+++ test/BufferView.spec.mjs
@@ -6,71 +6,80 @@ import {BufferView} from '../src/util/BufferView.mjs'
 
 describe('BufferView', () => {
 
-	it(`new (Uint8Array)`, async () => {
-		let uint8 = new Uint8Array(3)
-		uint8[2] = 5
-		let view = new BufferView(uint8)
-		assert.equal(view.byteLength, 3)
-		assert.equal(view.getUint8(0), uint8[0])
-		assert.equal(view.getUint8(1), uint8[1])
-		assert.equal(view.getUint8(2), uint8[2])
-		assert.equal(view.getUint8(2), 5)
+	describe('constructor new BufferView', () => {
+
+		it(`Uint8Array`, async () => {
+			let uint8 = new Uint8Array(3)
+			uint8[2] = 5
+			let view = new BufferView(uint8)
+			assert.equal(view.byteLength, 3)
+			assert.equal(view.getUint8(0), uint8[0])
+			assert.equal(view.getUint8(1), uint8[1])
+			assert.equal(view.getUint8(2), uint8[2])
+			assert.equal(view.getUint8(2), 5)
+		})
+
+		it(`number: creates new view`, async () => {
+			let view = new BufferView(3)
+			assert.equal(view.byteLength, 3)
+		})
+
+		it(`number: creates new view`, async () => {
+			let view = new BufferView(3)
+			assert.equal(view.byteLength, 3)
+		})
+
+		isNode && it(`Buffer.allocUnsafe: creates new view`, async () => {
+			let buffer = Buffer.allocUnsafe(3)
+			let val0 = buffer[0]
+			let val1 = buffer[1]
+			let val2 = buffer[2]
+			let view = new BufferView(buffer)
+			assert.equal(view.byteLength, 3)
+			assert.equal(view.getUint8(0), val0)
+			assert.equal(view.getUint8(1), val1)
+			assert.equal(view.getUint8(2), val2)
+		})
+
+		isNode && it(`Buffer.allocUnsafe, 0, 3: creates subview`, async () => {
+			let buffer = Buffer.allocUnsafe(5)
+			let val0 = buffer[0]
+			let val1 = buffer[1]
+			let val2 = buffer[2]
+			let view = new BufferView(buffer, 0, 3)
+			assert.equal(view.byteLength, 3)
+			assert.equal(view.getUint8(0), val0)
+			assert.equal(view.getUint8(1), val1)
+			assert.equal(view.getUint8(2), val2)
+		})
+
+		isNode && it(`Buffer.allocUnsafe, 1, 3: creates subview`, async () => {
+			let buffer = Buffer.allocUnsafe(5)
+			let val1 = buffer[1]
+			let val2 = buffer[2]
+			let val3 = buffer[3]
+			let view = new BufferView(buffer, 1, 3)
+			assert.equal(view.byteLength, 3)
+			assert.equal(view.getUint8(0), val1)
+			assert.equal(view.getUint8(1), val2)
+			assert.equal(view.getUint8(2), val3)
+		})
+
+		isNode && it(`Buffer.allocUnsafe, 2, 3: creates subview`, async () => {
+			let buffer = Buffer.allocUnsafe(5)
+			let val2 = buffer[2]
+			let val3 = buffer[3]
+			let val4 = buffer[4]
+			let view = new BufferView(buffer, 2, 3)
+			assert.equal(view.byteLength, 3)
+			assert.equal(view.getUint8(0), val2)
+			assert.equal(view.getUint8(1), val3)
+			assert.equal(view.getUint8(2), val4)
+		})
+
 	})
 
-	it(`new (number) creates new view`, async () => {
-		let view = new BufferView(3)
-		assert.equal(view.byteLength, 3)
-	})
-
-	isNode && it(`new (Buffer.allocUnsafe) creates new view`, async () => {
-		let buffer = Buffer.allocUnsafe(3)
-		let val0 = buffer[0]
-		let val1 = buffer[1]
-		let val2 = buffer[2]
-		let view = new BufferView(buffer)
-		assert.equal(view.byteLength, 3)
-		assert.equal(view.getUint8(0), val0)
-		assert.equal(view.getUint8(1), val1)
-		assert.equal(view.getUint8(2), val2)
-	})
-
-	isNode && it(`new (Buffer.allocUnsafe, 0, 3) creates subview`, async () => {
-		let buffer = Buffer.allocUnsafe(5)
-		let val0 = buffer[0]
-		let val1 = buffer[1]
-		let val2 = buffer[2]
-		let view = new BufferView(buffer, 0, 3)
-		assert.equal(view.byteLength, 3)
-		assert.equal(view.getUint8(0), val0)
-		assert.equal(view.getUint8(1), val1)
-		assert.equal(view.getUint8(2), val2)
-	})
-
-	isNode && it(`new (Buffer.allocUnsafe, 1, 3) creates subview`, async () => {
-		let buffer = Buffer.allocUnsafe(5)
-		let val1 = buffer[1]
-		let val2 = buffer[2]
-		let val3 = buffer[3]
-		let view = new BufferView(buffer, 1, 3)
-		assert.equal(view.byteLength, 3)
-		assert.equal(view.getUint8(0), val1)
-		assert.equal(view.getUint8(1), val2)
-		assert.equal(view.getUint8(2), val3)
-	})
-
-	isNode && it(`new (Buffer.allocUnsafe, 2, 3) creates subview`, async () => {
-		let buffer = Buffer.allocUnsafe(5)
-		let val2 = buffer[2]
-		let val3 = buffer[3]
-		let val4 = buffer[4]
-		let view = new BufferView(buffer, 2, 3)
-		assert.equal(view.byteLength, 3)
-		assert.equal(view.getUint8(0), val2)
-		assert.equal(view.getUint8(1), val3)
-		assert.equal(view.getUint8(2), val4)
-	})
-
-	isNode && it(`trying to create subview with offset/length outside of range throw`, async () => {
+	isNode && it(`trying to create subview with offset/length outside of range throws`, async () => {
 		let uint8 = new Uint8Array(5)
 		assert.throws(() => new BufferView(uint8, 2, 10))
 	})
