a1786c2b598d0ccc7751f34e921b623d41a2aeb2
Arnaud Jezequel <Arnaud.jezequel@gmail.com>
2022-07-13 09:02:59 +0200

    add Vite support
    Add vite-ignore in import module

--- src/util/import.mjs
+++ src/util/import.mjs
@@ -7,7 +7,7 @@ export default function(name, wrapper = pass) {
 		try {
 			return typeof require === 'function'
 				? Promise.resolve(wrapper(require(name)))
-				: import(/* webpackIgnore: true */ name).then(wrapper)
+				: import(/* webpackIgnore: true */ /* @vite-ignore */ name).then(wrapper)
 		} catch(err) {
 			console.warn(`Couldn't load ${name}`)
 		}
