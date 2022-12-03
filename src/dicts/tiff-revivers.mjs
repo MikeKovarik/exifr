ac1ad0544e53c228a19782a198dabc702c4d1364
websocket98765 <41795874+websocket98765@users.noreply.github.com>
2022-12-03 09:46:44 +0530

    Fix tiff-reviver to adjust from local time to UTC

--- src/dicts/tiff-revivers.mjs
+++ src/dicts/tiff-revivers.mjs
@@ -45,7 +45,7 @@ function reviveVersion(bytes) {
 	return array.join('.')
 }
 
-// can be '2009-09-23 17:40:52 UTC' or '2010:07:06 20:45:12'
+// can be '2009-09-23 17:40:52 UTC', '2010:07:06 20:45:12', or '2009-09-23 11:40:52-6:00'
 function reviveDate(string) {
 	if (typeof string !== 'string') return undefined
 	var [year, month, day, hours, minutes, seconds] = string.trim().split(/[-: ]/g).map(Number)
@@ -54,6 +54,13 @@ function reviveDate(string) {
 		date.setHours(hours)
 		date.setMinutes(minutes)
 		date.setSeconds(seconds)
+	}
+	// If offset exists, this is in local time. Adjust to UTC using the offset.
+	var result = string.match(/-[0-9]{2}:[0-9]{2}/)
+	if (result) {
+		var [offsetHH, offsetMM] = result[0].split(':')
+		var offsetMin = (Number(offsetHH) * 60) + Number(offsetMM)
+		date.setMinutes(date.getMinutes() - offsetMin)
 	}
 	if (Number.isNaN(+date))
 		return string
